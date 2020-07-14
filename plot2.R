ba74328d93023a27b8231d20dea5b80be621c692
Kanishka Goyal <kanishkagoyal13@gmail.com>
2020-07-14 03:57:59 -0700

    plot2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+b_total_emissions <- NEI %>%
+  filter(fips == "24510") %>%
+  group_by(year) %>%
+  summarise(Emissions = sum(Emissions))
+png(filename = "plot2.png")
+with(b_total_emissions,
+     barplot(height=Emissions/1000, names.arg = year, col = color_range, 
+             xlab = "Year", ylab = expression('PM'[2.5]*' in Kilotons'),
+             main = expression('Baltimore, Maryland Emissions from 1999 to 2008'))
+)
+dev.off()
